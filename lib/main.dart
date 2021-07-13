8eebc87c452b8792de9acfbf118d75e5a6b58c30
liamboston <liam.boston@yahoo.com>
2021-07-13 17:17:20 -0400

    added an intro statement to the HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -29,21 +29,31 @@ class _HomePageState extends State<HomePage> {
                     fontSize: 30.0,
                     fontWeight: FontWeight.bold,
                     color: Color(0xFF473D3A))),
-                    Padding(padding: EdgeInsets.only(right: 15.0),
-                      child: Container(
-                        height: 40.0,
-                        width: 40.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          image: DecorationImage(
-                            image: AssetImage("assets/profile.jpg"),
-                            fit: BoxFit.cover
-                          )
-                        ),
-                      )
-                    )
+            Padding(
+                padding: EdgeInsets.only(right: 15.0),
+                child: Container(
+                  height: 40.0,
+                  width: 40.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      image: DecorationImage(
+                          image: AssetImage("assets/profile.jpg"),
+                          fit: BoxFit.cover)),
+                ))
           ],
-        )
+        ),
+        SizedBox(height: 10.0),
+        Padding(
+            padding: const EdgeInsets.only(right: 45.0),
+            child: Container(
+              child: Text(
+                  "Let's select the best taste for your next coffee break!",
+                  style: TextStyle(
+                      fontFamily: "nunito",
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w300,
+                      color: Color(0xFFB0AAA7))),
+            ))
       ],
     )));
   }
