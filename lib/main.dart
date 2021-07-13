f93cbad88bc4c08b45b3101defae4114f53d9c2b
liamboston <liam.boston@yahoo.com>
2021-07-13 16:55:26 -0400

    added a header to the HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(HomePage());
+}
+
 class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -10,8 +14,24 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-    );
+    return MaterialApp(
+        home: Scaffold(
+            body: ListView(
+      padding: EdgeInsets.only(left: 15.0),
+      children: [
+        SizedBox(height: 50.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Welcome, Liam",
+                style: TextStyle(
+                    fontFamily: 'varela',
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF473D3A)))
+          ],
+        )
+      ],
+    )));
   }
 }
