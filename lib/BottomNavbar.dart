f0ca28a4a049de489690cc6db590918e9f857ce8
liamboston <liam.boston@yahoo.com>
2021-07-20 10:17:46 -0400

    minor tweaks to the BottomNavbar

--- lib/BottomNavbar.dart
+++ lib/BottomNavbar.dart
@@ -3,20 +3,26 @@ import 'package:flutter/material.dart';
 class BottomNavbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 65.0,
-      padding: EdgeInsets.only(top: 5.0, bottom: 30.0),
-      color: Color(0xFFDAB68C),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          IconButton(
-            onPressed: () {
-              Navigator.pushReplacementNamed(context, "/");
-            },
-            icon: Icon(Icons.home, size: 30.0),
-          ),
-        ],
+    return Padding(
+      padding: EdgeInsets.only(top: 8.0),
+      child: Container(
+        height: 55.0,
+        padding: EdgeInsets.only(
+          top: 3.0,
+          bottom: 25.0,
+        ),
+        color: Color(0xFFDAB68C),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, "/");
+              },
+              icon: Icon(Icons.home, size: 40.0),
+            ),
+          ],
+        ),
       ),
     );
   }
