db2250a2f9731bbfeb0250b98893e06180da9239
liamboston <liam.boston@yahoo.com>
2021-07-20 14:29:41 -0400

    added a white/pink background to the DetailsPage

--- lib/DetailsPage.dart
+++ lib/DetailsPage.dart
@@ -10,6 +10,37 @@ class DetailsPage extends StatefulWidget {
 class _DetailsPageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height - 20.0,
+                  width: MediaQuery.of(context).size.width,
+                  color: Color(0xFFF3B2B7),
+                ),
+                Positioned(
+                  top: MediaQuery.of(context).size.height / 2,
+                  child: Container(
+                    height: (MediaQuery.of(context).size.height / 2) - 20.0,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(40.0),
+                        topLeft: Radius.circular(40.0),
+                      ),
+                      color: Colors.white
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
