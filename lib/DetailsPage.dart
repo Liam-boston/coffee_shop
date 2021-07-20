258a2fe57cb0da7bd4b8b29ed0391bc0619f4869
liamboston <liam.boston@yahoo.com>
2021-07-20 12:17:08 -0400

    created a DetailsPage widget

---
+++ lib/DetailsPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _DetailsPageState();
+  }
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
