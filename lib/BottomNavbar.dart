1fbd1929d35297b9ffd38bafd272438a8a1fab92
liamboston <liam.boston@yahoo.com>
2021-07-20 12:14:28 -0400

    added a label to the home icon

--- lib/BottomNavbar.dart
+++ lib/BottomNavbar.dart
@@ -1,28 +1,34 @@
 import 'package:coffee_shop/presentation/app_icons.dart';
 import 'package:flutter/material.dart';
 
-
 class BottomNavbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(top: 8.0),
       child: Container(
-        height: 55.0,
+        height: 75.0,
         padding: EdgeInsets.only(
           top: 3.0,
-          bottom: 25.0,
+          bottom: 3.0,
         ),
         color: Color(0xFFDAB68C),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            IconButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, "/");
-              },
-              icon: Icon(AppIcons.home, size: 40.0),
-            ),
+            Column(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, "/");
+                  },
+                  icon: Icon(AppIcons.home, size: 30.0),
+                ),
+                Center(
+                  child: Text("Home"),
+                ),
+              ],
+            )
           ],
         ),
       ),
