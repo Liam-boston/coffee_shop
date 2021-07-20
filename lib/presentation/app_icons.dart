6a6b0bfbb7efe4a62f5ff73a912c1cd96e8bcc2f
liamboston <liam.boston@yahoo.com>
2021-07-20 11:26:31 -0400

    downloaded custom icons for the BottomNavbar

---
+++ lib/presentation/app_icons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class AppIcons {
+  AppIcons._();
+
+  static const _kFontFam = 'AppIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData home = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData profile = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData orders = IconData(0xe848, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
