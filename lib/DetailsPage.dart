77631c7515fb90c07c7fd6d36be1c4a2b8998f08
liamboston <liam.boston@yahoo.com>
2021-07-21 07:27:39 -0400

    added 5 IngredientItems to the DetailsPage

--- lib/DetailsPage.dart
+++ lib/DetailsPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:coffee_shop/presentation/ingredient_icons.dart';
+
 class DetailsPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -27,19 +29,134 @@ class _DetailsPageState extends State<DetailsPage> {
                     height: (MediaQuery.of(context).size.height / 2) - 20.0,
                     width: MediaQuery.of(context).size.width,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(40.0),
-                        topLeft: Radius.circular(40.0),
-                      ),
-                      color: Colors.white
-                    ),
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(40.0),
+                          topLeft: Radius.circular(40.0),
+                        ),
+                        color: Colors.white),
                   ),
                 ),
 
+                // content
+                Positioned(
+                  top: (MediaQuery.of(context).size.height / 2) + 25.0,
+                  left: 25.0,
+                  child: Container(
+                    height: (MediaQuery.of(context).size.height / 2) - 50.0,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListView(
+                      children: [
+                        Text(
+                          "Preparation time",
+                          style: TextStyle(
+                            fontFamily: "nunito",
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF726B68),
+                          ),
+                        ),
+                        SizedBox(height: 7.0),
+                        Text(
+                          "5min",
+                          style: TextStyle(
+                            fontFamily: "nunito",
+                            fontSize: 14.0,
+                            color: Color(0xFFC6C4C4),
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 15.0),
+                          child: Container(
+                            height: 0.5,
+                            color: Color(0xFFC6C4C4),
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          "Ingredients",
+                          style: TextStyle(
+                            fontFamily: "nunito",
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF726B68),
+                          ),
+                        ),
+                        SizedBox(height: 20.0),
+                        Container(
+                          height: 110.0,
+                          padding: EdgeInsets.only(right: 25.0),
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            children: [
+                              buildIngredientItem(
+                                "Water",
+                                Icon(IngredientIcons.water, size: 25.0, color: Colors.white),
+                                Color(0xFF6FC5DA),
+                              ),
+                              buildIngredientItem(
+                                "Ice",
+                                Icon(IngredientIcons.ice_cube, size: 25.0, color: Colors.white),
+                                Color(0xFFF8B870),
+                              ),
+                              buildIngredientItem(
+                                "Brewed Espresso",
+                                Icon(Icons.coffee_rounded, size: 25.0, color: Colors.white),
+                                Color(0xFF615955),
+                              ),
+                              buildIngredientItem(
+                                "Sugar",
+                                Icon(IngredientIcons.sugar, size: 25.0, color: Colors.white),
+                                Color(0xFFF39595),
+                              ),
+                              buildIngredientItem(
+                                "Hazelnut Syrup",
+                                Icon(IngredientIcons.hazelnut, size: 25.0, color: Colors.white),
+                                Color(0xFF8FC28A),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  buildIngredientItem(String name, Icon iconName, Color bgColor) {
+    return Padding(
+      padding: EdgeInsets.only(right: 10.0),
+      child: Column(
+        children: [
+          Container(
+            height: 50.0,
+            width: 50.0,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0), color: bgColor),
+            child: Center(child: iconName),
+          ),
+          SizedBox(height: 4.0),
+          Container(
+            width: 80.0,
+            child: Center(
+              child: Text(
+                name,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: "nunito",
+                  fontSize: 12.0,
+                  color: Color(0xFFC2C0C0),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
