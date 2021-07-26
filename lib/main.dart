acf3b4dd43b014434affbcd2b9fd489ea97cf418
liamboston <liam.boston@yahoo.com>
2021-07-26 10:18:06 -0400

    related to #3 - added routes in main.dart to the EspressoConPanna and BlondeVanillaLatte widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:coffee_shop/Home/HomePage.dart';
 import 'package:coffee_shop/Details/CaramelMacchiato.dart';
+import 'package:coffee_shop/Details/EspressoConPanna.dart';
+import 'package:coffee_shop/Details/BlondeVanillaLatte.dart';
 
 void main() {
   runApp(CoffeeShop());
@@ -16,6 +18,8 @@ class CoffeeShop extends StatelessWidget {
       routes: {
         "/": (context) => HomePage(),
         "/caramel_macchiato": (context) => CaramelMacchiato(),
+        "/espresso_con_panna": (context) => EspressoConPanna(),
+        "/blonde_vanilla_latte": (context) => BlondeVanillaLatte()
       },
     );
   }
